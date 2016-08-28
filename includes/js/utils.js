b23f265bfcbe18ef998a45fc1370cd7ef8809d83
System Administrator <root@Amits-Air.lan>
2016-08-28 13:34:57 -0500

    Changing decimal values for currency with labels

--- includes/js/utils.js
+++ includes/js/utils.js
@@ -1,39 +1,62 @@
 
 roiCalculator.directive('format', ['$filter', function ($filter) {
     return {
-            /*require: '?ngModel',
+            require: '?ngModel',
             link: function (scope, elem, attrs, ctrl) {
                 if (!ctrl) return;
 
 
                 ctrl.$formatters.unshift(function (a) {
-                    return $filter(attrs.format)(ctrl.$modelValue)
+                    var returnVal = rounding.FormatAsThousands(Number(ctrl.$modelValue),2);
+                    return returnVal;
+                    //return $filter(attrs.format)(ctrl.$modelValue)
                 });
 
 
                 ctrl.$parsers.unshift(function (viewValue) {
-                    var plainNumber = viewValue.replace(/[^\d|\-+|\.+]/g, '');
-                    elem.val($filter(attrs.format)(plainNumber));
+                    /*var returnVal = rounding.FormatAsThousands(Number(viewValue),2);
+                    console.log(returnVal);
+                    return returnVal;*/
+                    var plainNumber = viewValue.toString().replace(/[^\d|\-+|\.+]/g, '');
+                    elem.val(plainNumber);
                     return plainNumber;
                 });
-            }*/
+            }
     };
 }]);
 
 $(function(){
-    $('input#decimal').on('input', function() {
-        this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
-    });
-
-    $('#number').keyup(function(e)
-                                {
-        if (/\D/g.test(this.value))
-        {
-            this.value = this.value.replace(/\D/g, '');
-        }
-    });
 
 });
+
+var rounding = (function () {
+  "use strict";
+    return {
+        FormatAsThousands: (function (amount,noOfPrecisions) {
+            amount = (noOfPrecisions > 0) ? this.roundDecimals(amount, noOfPrecisions) : Math.round(amount);
+            var returnVal = this.addSeparatorsNF(amount,'.','.',',');
+            return returnVal;
+        }),
+        addSeparatorsNF: (function (nStr, inD, outD, sep) {
+            nStr += '';
+            var dpos = nStr.indexOf(inD);
+            var nStrEnd = '';
+            if (dpos != -1) {
+                nStrEnd = outD + nStr.substring(dpos + 1, nStr.length);
+                nStr = nStr.substring(0, dpos);
+            }
+            var rgx = /(\d+)(\d{3})/;
+            while (rgx.test(nStr)) {
+                nStr = nStr.replace(rgx, '$1' + sep + '$2');
+            }
+            return nStr + nStrEnd;
+        }),
+        roundDecimals : (function(num, noOfPrecisions){
+            var precision = Math.pow(10,noOfPrecisions);
+            return Math.round(num * precision) / precision;
+        })
+   };
+}());
 
 function roundDecimals(num, noOfPrecisions){
     var precision = Math.pow(10,noOfPrecisions);
@@ -41,23 +64,11 @@ function roundDecimals(num, noOfPrecisions){
 }
 
 function addSeparatorsNF(nStr, inD, outD, sep){
-    nStr += '';
-    var dpos = nStr.indexOf(inD);
-    var nStrEnd = '';
-    if (dpos != -1) {
-        nStrEnd = outD + nStr.substring(dpos + 1, nStr.length);
-        nStr = nStr.substring(0, dpos);
-    }
-    var rgx = /(\d+)(\d{3})/;
-    while (rgx.test(nStr)) {
-        nStr = nStr.replace(rgx, '$1' + sep + '$2');
-    }
-    return nStr + nStrEnd;
+    
 }
 
 function FormatAsThousands(amount, noOfPrecisions){
-    amount = (noOfPrecisions > 0) ? roundDecimals(amount, noOfPrecisions) : Math.round(amount);
-    return addSeparatorsNF(amount,'.','.',',');
+    
 }
 
 function validateQty(event) {
@@ -70,4 +81,4 @@ function validateQty(event) {
         return false;
     }
     else return true;
-};
+}
