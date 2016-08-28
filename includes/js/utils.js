c78a1aedf38a9dbb0cc7dff7fe7967c878518eb5
System Administrator <root@Amits-Air.lan>
2016-08-28 01:55:08 -0500

    Adding new changes

--- includes/js/utils.js
+++ includes/js/utils.js
@@ -1,22 +1,22 @@
 
 roiCalculator.directive('format', ['$filter', function ($filter) {
     return {
-        /*require: '?ngModel',
-        link: function (scope, elem, attrs, ctrl) {
-            if (!ctrl) return;
+            /*require: '?ngModel',
+            link: function (scope, elem, attrs, ctrl) {
+                if (!ctrl) return;
 
 
-            ctrl.$formatters.unshift(function (a) {
-                return $filter(attrs.format)(ctrl.$modelValue)
-            });
+                ctrl.$formatters.unshift(function (a) {
+                    return $filter(attrs.format)(ctrl.$modelValue)
+                });
 
 
-            ctrl.$parsers.unshift(function (viewValue) {
-                var plainNumber = viewValue.replace(/[^\d|\-+|\.+]/g, '');
-                elem.val($filter(attrs.format)(plainNumber));
-                return plainNumber;
-            });
-        }*/
+                ctrl.$parsers.unshift(function (viewValue) {
+                    var plainNumber = viewValue.replace(/[^\d|\-+|\.+]/g, '');
+                    elem.val($filter(attrs.format)(plainNumber));
+                    return plainNumber;
+                });
+            }*/
     };
 }]);
 
